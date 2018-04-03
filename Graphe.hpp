6abf2effa2fa5ea4e07782da3c720db268e9fdfb
Albichte <36403572+Albichte@users.noreply.github.com>
2018-04-03 18:08:22 +0200

    Version 1.0 graphe.hpp

---
+++ Graphe.hpp
@@ -0,0 +1,31 @@
+#ifndef Graphe
+#define Graphe
+#include <vector>
+using namespace std;
+
+class Sommet
+class Arc
+
+
+class Graphe
+{
+private:
+  vector<Sommet> m_sommets;
+  vector<Arc> m_arcs;
+  int m_ordre;
+  string m_nom;
+public:
+  Graphe(); /* par défaut lors de l'implémentation d'un nouveau graphe par un utilisateur */
+  Graphe(string no_fich); // ouverture graphe
+  ~Graphe() // destruteur des vecteurs d'arcs et de sommets. Penser à appeler la sauvegarde.
+  void reset_marquages_som();// fait un reset de marquages des sommets à false
+  void reset_actif_som(); // fait un reset des valeur de actif de tous les sommets à true
+  void reset_marquages_arc();// fait un reset de marquages des arcs à false
+  void reset_actif_arc(); // fait un reset des valeur de actif de tous les arcs à true
+  void sauvegarde();
+
+
+
+};
+
+#endif /* Graphe */
