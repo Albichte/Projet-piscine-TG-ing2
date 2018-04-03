094cc5ee6f553888249c0a5cf4c548c6e779990a
Albichte <36403572+Albichte@users.noreply.github.com>
2018-04-03 18:28:51 +0200

    Version 1.1

---
+++ Sommet.hpp
@@ -0,0 +1,48 @@
+#ifndef Sommet_hpp
+#define Sommet_hpp
+#include <string>
+#include <vector>
+#include <iostream>
+
+class Sommet
+{
+private:
+  string m_nom_im
+  int m_num_sommet
+  vector <Sommet *>  m_prede
+  vector <Sommet *> m_succe
+  int m_deg // = nb_succe + sb_prede
+  bool m_actif
+  bool m_marque
+  int m_x , m_y // coord allegro
+  int m_stable // stabilité de la population ou K
+  float m_N // population ou biomasse
+  float m_rythme // correspond à r
+  vector <int> m_N_histo /* sauvegarde les valeurs de m_N lors d'une simulation */
+  string m_nom_im;
+  int m_num_sommet;
+  vector <Sommet *>  m_prede;
+  vector <Sommet *> m_succe;
+  int m_deg; // = nb_succe + sb_prede
+  bool m_actif;
+  bool m_marque;
+  int m_x , m_y; // coord allegro
+  int m_stable; // stabilité de la population ou K
+  float m_N; // population ou biomasse
+  float m_rythme; // correspond à r
+  vector <int> m_N_histo; /* sauvegarde les valeurs de m_N lors d'une simulation */
+public:
+  Sommet()
+  ~Sommet() // destruction pointeurs voisins
+  int vol_n1_som() /* retourne la val de un+1, et stoque dans l'historique
+  Sommet();
+  ~Sommet(); // destruction pointeurs voisins
+  int vol_n1_som(); /* retourne la val de un+1, et stoque dans l'historique
+  un et un+1 (pas grave si on stoque la même val 2* au même endroit */
+  void reset_N_histo() // on vide l'historique des valeurs de m_N
+  void reset_N_histo(); // on vide l'historique des valeurs de m_N
+
+
+};
+
+#endif
